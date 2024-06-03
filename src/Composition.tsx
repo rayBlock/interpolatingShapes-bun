8a55fb339015907cb23636e6b3ff0c3c336791f1
Raysayhey <rlotmar@hotmail.com>
2024-06-03 19:42:26 +0200

    svg bounding box ignores stroke ?

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,6 +1,6 @@
 import { makeEllipse, makeStar } from '@remotion/shapes';
 import { AbsoluteFill, interpolate, useCurrentFrame } from 'remotion';
-import { interpolatePath } from "@remotion/paths";
+import { getBoundingBox, interpolatePath } from "@remotion/paths";
 import { evolvePath } from "@remotion/paths";
 
 import { z } from 'zod';
@@ -36,10 +36,11 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = () => {
   const interpolated = interpolatePath(inerpolatePath, starpath, ellips);
   const evol = evolvePath(evolveInterpolate, interpolated);
 
+  const bounding = getBoundingBox(interpolated);
+
   return (
     <AbsoluteFill className="bg-gray-100 items-center justify-center">
-      <svg viewBox='0 0 720 720' style={{
-        margin: '40px'
+      <svg viewBox={`0 0 ${bounding.width} ${bounding.height }`} style={{
 
       }}>
         <path style={{
@@ -47,7 +48,7 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = () => {
           strokeDasharray: evol.strokeDasharray,
           strokeDashoffset: evol.strokeDashoffset,
           strokeWidth: 10,
-          strokeLinecap: 'square',
+          strokeLinecap: 'round',
 
         }} d={interpolated} />
       </svg>
